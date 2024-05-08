0eef6dee4b2e9ad127cfd55849dbc94391b602b9
Oron Paz <oron.paz26@gmail.com>
2024-05-08 21:35:49 +0300

    added components and pages folders

---
+++ ru-single/src/pages/About.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '../components/Header.jsx';
+import Footer from '../components/Footer.jsx';
+
+const About = () => {
+  return (
+    <div>
+      <Header/>
+        <h1>About Page</h1>
+        <p>Welcome to the About page!</p>
+      <Footer/>
+    </div>
+  );
+};
+
+export default About;
---
+++ ru-single/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
+const About = () => {
+  return (
+    <div>
+      <Header/>
+        <h1>About Page</h1>
+        <p>Welcome to the HOME page!</p>
+      <Footer/>
+    </div>
+  );
+};
+
+export default About;
