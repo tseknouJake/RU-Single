110963096a61859cfe1f18f89a19ff9f96d889d3
Oron Paz <oron.paz26@gmail.com>
2024-05-08 19:37:53 +0300

    initalized react project

---
+++ ru-single/src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          THIS IS THE HOMEPAGE FOR RU-SINGLE
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
