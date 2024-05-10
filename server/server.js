43900f73ae277831d19cb061e9640ffeb5e8e482
Oron Paz <oron.paz26@gmail.com>
2024-05-10 23:11:21 +0300

    made some edits to the server.js

--- server/server.js
+++ server/server.js
@@ -3,13 +3,13 @@ const app = express();
 const mongoose = require('mongoose');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const env = require('dotenv').config();
+//const User = require('./schema/User');  // Import your User schema
+const router = express.Router();
 
 const uri = "mongodb+srv://OronPaz:" + process.env.MongoDBPassword +"@ru-single.63vvotl.mongodb.net/?retryWrites=true&w=majority&appName=ru-single";
-console.log(process.env.MongoDBPassword);
 
 app.listen(5050, () => {
   console.log('Server is running on port 5050');
-  
 })
 
 const client = new MongoClient(uri, {
@@ -20,16 +20,14 @@ const client = new MongoClient(uri, {
   }
 });
 
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
+client.connect(()=>  {
+  const database = client.db('ru-single');
+  const users = database.collection('users');
+  console.log('Connected to the database');
+}).then(() => { 
+  console.log('Connected to the database');
+} ).catch((error) => {
+  console.error('Error connecting to the database:', error);
+});
+
+
