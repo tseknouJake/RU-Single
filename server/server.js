8e4385320064538906b79078d6d55c1875c87889
Oron Paz <oron.paz26@gmail.com>
2024-05-10 16:55:23 +0300

    connected mongoDB

--- server/server.js
+++ server/server.js
@@ -1,10 +1,35 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const env = require('dotenv').config();
 
-app.get('/api', (req, res) => {
-  res.json({ message: 'Hello from server!' });
-});
+const uri = "mongodb+srv://OronPaz:" + process.env.MongoDBPassword +"@ru-single.63vvotl.mongodb.net/?retryWrites=true&w=majority&appName=ru-single";
+console.log(process.env.MongoDBPassword);
 
 app.listen(5050, () => {
   console.log('Server is running on port 5050');
+  
 })
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
