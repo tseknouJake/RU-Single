d0ad580938e107cb286c52c3f2cda6210b2a81b0
Oron Paz <oron.paz26@gmail.com>
2024-05-12 11:43:28 +0300

    added routing for login page

--- ru-single/src/App.js
+++ ru-single/src/App.js
@@ -3,6 +3,7 @@ import { Routes } from 'react-router-dom';
 import Signup from './pages/Signup';
 import Home from './pages/Home';
 import About from './pages/About';
+import Login from './pages/Login';
 import './App.css';
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </Router>
     </div>
