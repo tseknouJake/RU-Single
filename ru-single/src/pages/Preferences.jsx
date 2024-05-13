d8a1cb41ac2c340c43fd8f8aa89b3a0404bd047c
Oron Paz <oron.paz26@gmail.com>
2024-05-13 17:38:21 +0300

    added checkbox for bi option

--- ru-single/src/pages/Preferences.jsx
+++ ru-single/src/pages/Preferences.jsx
@@ -35,11 +35,29 @@ const SignupAdditional = () => {
       <form onSubmit={handleSubmit}>
         <div>
           <label htmlFor="gender">Gender:</label>
-          <input type="text" id="gender" name="gender" value={formData.gender} onChange={handleChange} />
+          <div>
+            <input type="checkbox" id="male" name="gender" value="Male" checked={formData.gender === "Male"} onChange={handleChange} />
+            <label htmlFor="male">Male</label>
+          </div>
+          <div>
+            <input type="checkbox" id="female" name="gender" value="Female" checked={formData.gender === "Female"} onChange={handleChange} />
+            <label htmlFor="female">Female</label>
+          </div>
         </div>
         <div>
           <label htmlFor="genderPreference">Gender Preference:</label>
-          <input type="text" id="genderPreference" name="genderPreference" value={formData.genderPreference} onChange={handleChange} />
+          <div>
+            <input type="checkbox" id="malePreference" name="genderPreference" value="Male" checked={formData.genderPreference === "Male"} onChange={handleChange} />
+            <label htmlFor="malePreference">Male</label>
+          </div>
+          <div>
+            <input type="checkbox" id="femalePreference" name="genderPreference" value="Female" checked={formData.genderPreference === "Female"} onChange={handleChange} />
+            <label htmlFor="femalePreference">Female</label>
+          </div>
+          <div>
+            <input type="checkbox" id="biPreference" name="genderPreference" value="Bi" checked={formData.genderPreference === "Bi"} onChange={handleChange} />
+            <label htmlFor="biPreference">Bi</label>
+          </div>
         </div>
         <button type="submit">Sign Up</button>
       </form>
