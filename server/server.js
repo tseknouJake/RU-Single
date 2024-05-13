ba5b826afd115a0571368272e950015a23c1e80f
Oron Paz <oron.paz26@gmail.com>
2024-05-13 11:07:59 +0300

    added preferences funcionlaoty to users in database

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const env = require('dotenv').config();
 const User = require('./schema/User.js');  // Import your User model
 const bodyParser = require('body-parser');
+const localStorage = require('localStorage');
 
 // Middleware to parse JSON bodies
 app.use(bodyParser.json());
@@ -33,8 +34,33 @@ app.get('/userData', async (req, res) => {
 
 // Define a route to create a new user
 app.post('/signup', async (req, res) => {
-  const { name, email, password } = req.body;
-  const user = new User({name, email, password});
-  await user.save();
-  res.send(user);
+  try {
+    const { name, email, password } = req.body;
+    const existingUser = await User.findOne({ email }); // Check if name or email already exists
+    if (existingUser) {
+      //console.error('Email already exists in database');
+      return res.status(400).json({ error: 'Email already exists' }); // Respond with error if name or email already exists
+    }
+    // Store basic information in session/local storage or database
+    localStorage.setItem('basicInfo', JSON.stringify({ name, email, password }));
+    res.sendStatus(200); // Respond with success status
+  } catch (error) {
+    console.error('Error storing basic information:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+app.post('/signup/preferences', async (req, res) => {
+  try {
+    const { gender, genderPreference } = req.body;
+    // Retrieve basic information from session/local storage or database
+    const basicInfo = JSON.parse(localStorage.getItem('basicInfo'));
+    const userData = { ...basicInfo, gender, genderPreference };
+    // Save complete user profile to the database
+    const newUser = await User.create(userData);
+    res.status(201).json(newUser); // Respond with created user data
+  } catch (error) {
+    console.error('Error creating user with additional details:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
 });
