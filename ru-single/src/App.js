ede8053155c818f40f0e0fa7dc9d8af46b7220e8
Oron Paz <oron.paz26@gmail.com>
2024-05-13 11:09:33 +0300

    added routing for preferences page

--- ru-single/src/App.js
+++ ru-single/src/App.js
@@ -4,6 +4,7 @@ import Signup from './pages/Signup';
 import Home from './pages/Home';
 import About from './pages/About';
 import Login from './pages/Login';
+import Preferences from './pages/Preferences';
 import './App.css';
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/signup/preferences" element={<Preferences />} />
         </Routes>
       </Router>
     </div>
